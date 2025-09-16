2d407cd58a155f81ab16cb3c60e86e91d395ab9e
erikyryang <erikyryang@gmail.com>
2025-09-16 20:32:04 -0300

    refactor: reorganize authentication classes and DTOs for improved structure and clarity

--- src/main/java/com/cine/sk/cinesk/controller/AuthController.java
+++ src/main/java/com/cine/sk/cinesk/controller/AuthController.java
@@ -1,11 +1,12 @@
 package com.cine.sk.cinesk.controller;
 
-import com.cine.sk.cinesk.controller.dto.AuthRequestDTO;
-import com.cine.sk.cinesk.controller.dto.AuthResponseDTO;
-import com.cine.sk.cinesk.controller.dto.ChangePasswordRequestDTO;
-import com.cine.sk.cinesk.controller.dto.RegisterRequestDTO;
-import com.cine.sk.cinesk.domain.user.AuthService;
-import com.cine.sk.cinesk.domain.user.UserEntity;
+import com.cine.sk.cinesk.domain.auth.dto.AuthRequestDTO;
+import com.cine.sk.cinesk.domain.auth.dto.AuthResponseDTO;
+import com.cine.sk.cinesk.domain.auth.dto.ChangePasswordRequestDTO;
+import com.cine.sk.cinesk.domain.auth.AuthService;
+import com.cine.sk.cinesk.domain.user.User;
+import com.cine.sk.cinesk.domain.user.dto.RegisterDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,25 +20,18 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthResponseDTO> register(@RequestBody RegisterRequestDTO request) {
-        AuthResponseDTO response = authService.register(request);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<AuthResponseDTO> register(@Valid @RequestBody RegisterDTO registerRequest) {
+        return authService.register(registerRequest);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<AuthResponseDTO> login(@RequestBody AuthRequestDTO request) {
-        AuthResponseDTO response = authService.login(request);
-        return ResponseEntity.ok(response);
+    @PostMapping(path = "/login")
+    public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody AuthRequestDTO authRequestDTO) {
+        return authService.login(authRequestDTO);
     }
 
     @PostMapping("/change-password")
-    public ResponseEntity<Void> changePassword(@RequestBody ChangePasswordRequestDTO request, @AuthenticationPrincipal UserEntity user) {
+    public ResponseEntity<Void> changePassword(@RequestBody ChangePasswordRequestDTO request, @AuthenticationPrincipal User user) {
         authService.changePassword(user, request);
         return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/me")
-    public ResponseEntity<UserEntity> getAuthenticatedUser(@AuthenticationPrincipal UserEntity user) {
-        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/cine/sk/cinesk/controller/dto/AuthRequestDTO.java
+++
@@ -1,3 +0,0 @@
-package com.cine.sk.cinesk.controller.dto;
-
-public record AuthRequestDTO(String email, String password) {}
