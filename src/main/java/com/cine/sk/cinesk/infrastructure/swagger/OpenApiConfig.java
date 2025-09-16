7d747165a1a6a812037733b6f63b83e8fee5a3e4
eriky <erikyryang@gmail.com>
2025-09-16 20:39:41 -0300

    refactor: update OpenAPI configuration and enhance security request matchers

--- src/main/java/com/cine/sk/cinesk/infrastructure/swagger/OpenApiConfig.java
+++ src/main/java/com/cine/sk/cinesk/infrastructure/swagger/OpenApiConfig.java
@@ -1,28 +1,19 @@
 package com.cine.sk.cinesk.infrastructure.swagger;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "CineSK API",
-                version = "1.0",
-                description = "API para o projeto CineSK.",
-                contact = @Contact(name = "Equipe de Desenvolvimento")
-        ),
-        security = @SecurityRequirement(name = "bearerAuth")
-)
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CineSK API")
+                        .version("1.0.0")
+                        .description("API para o projeto CineSK"));
+    }
 }
