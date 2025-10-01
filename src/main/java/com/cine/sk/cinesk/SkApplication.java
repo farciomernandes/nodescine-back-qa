90f10f68c518c8e7ff0f203c2290a77fce45b24b
farciomernandes <farciomernandes@gmail.com>
2025-10-01 14:23:54 -0300

    feat: update pom.xml and env

--- src/main/java/com/cine/sk/cinesk/SkApplication.java
+++ src/main/java/com/cine/sk/cinesk/SkApplication.java
@@ -4,14 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
 public class SkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
 
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+        SpringApplication.run(SkApplication.class, args);
+    }
 }
