0f26d84b3c8964da1858f2deedfba1c545aaaa5c
erikyryan <erikyryang@gmail.com>
2025-11-03 18:25:20 -0300

    add transaction

--- src/main/java/com/cine/sk/cinesk/domain/user/dto/TransactionDTO.java
+++ src/main/java/com/cine/sk/cinesk/domain/user/dto/TransactionDTO.java
@@ -4,12 +4,16 @@ import com.cine.sk.cinesk.domain.movie.Movie;
 import com.cine.sk.cinesk.domain.transaction.payment.OrderStatusEnum;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Builder
 public class TransactionDTO {
 
     private Long transactionId;
+
+    private LocalDateTime createdAt;
 
     private OrderStatusEnum status;
 
