046c69c57cd4eb0f306c05afe81027ca664139ac
erikyryan <erikyryang@gmail.com>
2025-12-18 10:32:55 -0300

    removing old banner

--- src/main/java/com/cine/sk/cinesk/infrastructure/repositorys/BannerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cine.sk.cinesk.infrastructure.repositorys;
-
-import com.cine.sk.cinesk.domain.banner.Banner;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BannerRepository extends JpaRepository<Banner, Long> {
-}
